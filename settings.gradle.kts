a4d046fa117f09678b2275f6b08c832edfd707ca
David Burström <david.burstrom@gmail.com>
2023-04-07 13:06:59 +0200

    Require JDK 11 or later for building

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,11 @@
 rootProject.name = "ConTester"
 
+if (!JavaVersion.current().isJava11Compatible) {
+    throw GradleException(
+        "The project requires JDK 11 or later to build. Current JDK is ${JavaVersion.current()}."
+    )
+}
+
 include(":benchmarks:with-driver")
 include(":benchmarks:without-driver")
 include(":breakpoint")
