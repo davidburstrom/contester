a5ebfdad52695aef0d021c755c5d174467593592
David Burström <david.burstrom@gmail.com>
2023-04-26 09:57:06 +0200

    Bump JUnit5 to 5.9.3

--- driver/build.gradle.kts
+++ driver/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 ext.set("mutationThreshold", 80)
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
 }
 
 java {
