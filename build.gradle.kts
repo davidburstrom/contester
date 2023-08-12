046355b94f095bf94de5e161644fcecf14d0a683
David Burström <david.burstrom@gmail.com>
2023-08-12 13:21:38 +0200

    Bump extended mutators to 1.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -141,7 +141,7 @@ allprojects {
                     }
                 }
                 dependencies {
-                    "pitest"("com.groupcdg.arcmutate:base:1.1.2")
+                    "pitest"("com.groupcdg.arcmutate:base:1.1.3")
                     "pitest"("com.groupcdg.pitest:pitest-accelerator-junit5:1.0.6")
                 }
                 tasks.named<PitestTask>("pitest").configure {
