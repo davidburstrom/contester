00620528bf7dd89a13c9c5d84dabfa083cffd394
David Burström <david.burstrom@gmail.com>
2023-09-02 10:09:31 +0200

    Bump Spotless to 6.21.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import info.solidsoft.gradle.pitest.PitestPluginExtension
 import info.solidsoft.gradle.pitest.PitestTask
 
 plugins {
-    id("com.diffplug.spotless") version "6.20.0"
+    id("com.diffplug.spotless") version "6.21.0"
     id("info.solidsoft.pitest") version "1.9.11" apply false
     id("com.github.ben-manes.versions") version "0.47.0"
     id("net.ltgt.errorprone") version "3.1.0" apply false
